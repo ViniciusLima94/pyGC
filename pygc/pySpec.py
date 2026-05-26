1734e7d09c684635fdbcbad75ee5b027e2307403
ViniciusLima94 <vinicius.lima.cordeiro@gmail.com>
2026-05-26 16:30:55 +0200

    update

--- pygc/pySpec.py
+++
@@ -1,5 +0,0 @@
-# Replaced by pygc.spectral_analysis.fourier (MNE-based, no neo/elephant dependency).
-# This file is kept for reference only.
-from .spectral_analysis.fourier import compute_freq, csd_fourier as cxy, morlet_transform as morlet, morlet_csd as morlet_power
-
-__all__ = ["compute_freq", "cxy", "morlet", "morlet_power"]
